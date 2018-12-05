21c6f677b855c44e28b9bb4268c60f7c196c6a90
lsriw <lsriw@debian>
2018-12-05 18:40:53 +0100

    Lab8 start

--- l5/binaryzacjaLokalnaSauvoli.m
+++ l5/binaryzacjaLokalnaSauvoli.m
@@ -9,7 +9,7 @@ for i = 1:X
         
         mean = meanLT(i, j, floor(frame/2), image, X, Y);
         stddev = stddevLT(i, j, floor(frame/2), image, mean, X, Y);
-        threshold = mean * (1 + k *(stddev/R - 1));
+        threshold = mean * (1 - k *(stddev/R - 1));
         
         if image(i,j) > threshold
             imageBW(i,j) = 255;
